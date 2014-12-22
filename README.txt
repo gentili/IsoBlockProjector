ff1d55e111b66da91f62c62a658eb87e18ae1277
Michael Gentili <gentili@mcpnet.ca>
2014-12-22 12:58:55 -0500

    Skeleton of mod

---
+++ README.txt
@@ -0,0 +1,8 @@
+This is the IsoBlockProjector Minecraft Mod
+-------------------------------------------
+
+This mod provides an interface to allow external applications
+to request isometric projections of the minecraft world.
+
+Note: When eclipse workspace breaks
+rm .metadata/.plugins/org.eclipse.core.resources/.snap
---
+++ src/main/java/ca/mcpnet/isoblockprojector/IsoBlockProjector.java
@@ -0,0 +1,18 @@
+package ca.mcpnet.isoblockprojector;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = IsoBlockProjector.MODID, version = IsoBlockProjector.VERSION)
+public class IsoBlockProjector
+{
+    public static final String MODID = "isoblockprojector";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
